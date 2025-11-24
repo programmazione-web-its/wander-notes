b167324b1441e406011c1170cad12556d3d30d94
Annalisa Antonioli <a.antonioli@edu-its.it>
2025-11-24 12:18:10 +0100

    add fetch to single

--- app/trips/[tripId]/page.js
+++ app/trips/[tripId]/page.js
@@ -1,5 +1,12 @@
-export default function TripPage({params}) {
+const getTrip = async (id) => {
+    const res = await fetch(`https://dummyjson.com/posts/${id}`)
+    return res.json()
+}
 
 
+export default async function TripPage({params}) {
+    const {tripId} = await params
+    const trip = await getTrip(tripId)
+    console.log("TRIP", trip)
     return <div className="container"><h1>Single Trip</h1></div>
 }
